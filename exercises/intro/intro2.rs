c1752e8d688ec424c9f5c69782d41e6f8bac1736
arminveres <armin.veres@hotmail.com>
2022-10-17 22:10:32 +0200

    Playing around

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -2,8 +2,12 @@
 // Make the code print a greeting to the world.
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::io;
 
 fn main() {
-    println!("Hello {}!");
+    let mut username = String::new();
+    io::stdin()
+        .read_line(&mut username)
+        .expect("failed to read name");
+    println!("Hello {username}!");
 }
